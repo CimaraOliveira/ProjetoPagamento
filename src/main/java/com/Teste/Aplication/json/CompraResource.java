750eaa3b34c1dd7013f1c782541a5e1fd1f0558e
ProjetoDSW <cleotads21@gmail.com>
2020-12-03 21:38:27 -0300

    Serviços de Compra

--- src/main/java/com/Teste/Aplication/json/CompraResource.java
+++ src/main/java/com/Teste/Aplication/json/CompraResource.java
@@ -1,6 +1,7 @@
 package com.Teste.Aplication.json;
 
 import java.security.Principal;
+import java.time.LocalDate;
 import java.util.Date;
 
 import javax.validation.Valid;
@@ -12,57 +13,104 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.Teste.Aplication.Enuns.TipoPagamento;
+import com.Teste.Aplication.model.Boleto;
+import com.Teste.Aplication.model.Cartao;
 import com.Teste.Aplication.model.Compra;
 import com.Teste.Aplication.model.User;
+import com.Teste.Aplication.service.BoletoService;
+import com.Teste.Aplication.service.CartaoService;
 import com.Teste.Aplication.service.CompraService;
 import com.Teste.Aplication.service.UserService;
 
+class TestBoleto { // Classe Auxiliar
+	public Boleto boleto;
+	public Long idCompra;
+}
+
+class TestCartao{ // Classe Auxiliar
+	public Cartao cartao;
+	public Long idCompra;
+}
+
 @RestController
 @RequestMapping("/api/compras")
-@CrossOrigin(origins = {"http://localhost:8080"})
+@CrossOrigin()
 public class CompraResource {
-	
+
 	@Autowired
 	private CompraService compraService;
-	
+
 	@Autowired
 	private UserService userService;
-	
-	
-	@PostMapping(produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-	public ResponseEntity<Compra> salvar(@Valid Compra compra,User user,
-			@RequestParam("tipoPagamento") TipoPagamento tipoPagamento){
-		
-		compra.setDataCompra(new Date());
-    	compra.setValor(compra.getValor() * compra.getQuantidade());
-		
-		if(tipoPagamento.equals(TipoPagamento.CARTAO)){
-			
-			compra.setId(compra.getId());
+	@Autowired
+	private BoletoService boletoService;
+	@Autowired
+	private CartaoService cartaoService;
+
+	@PostMapping("/saveBoleto")
+	public ResponseEntity<Boleto> salvarBoleto(@RequestBody TestBoleto test) {
+		Compra compra = compraService.findByIdCompra(test.idCompra);
+
+		if (compra != null) {
+			boletoService.salvarBoleto(test.boleto);
+			test.boleto.setDataCompra(LocalDate.now());
+			compra.setBoleto(test.boleto);
 			compraService.salvarCompra(compra);
-			return new ResponseEntity<Compra>(compra,HttpStatus.OK);
-						
-		}else if(tipoPagamento.equals(TipoPagamento.BOLETO)){
-			
-			compra.setId(compra.getId());
+			return ResponseEntity.status(HttpStatus.CREATED).body(test.boleto);
+		}
+
+		return ResponseEntity.status(400).build();
+	}
+
+	@PostMapping("/saveCartao")
+	public ResponseEntity<Cartao> salvarCartao(@RequestBody TestCartao test){
+		Compra compra = compraService.findByIdCompra(test.idCompra);
+		
+		if(compra != null) {
+			cartaoService.salvarCartao(test.cartao);
+			compra.setCartao(test.cartao);
 			compraService.salvarCompra(compra);
-			return new ResponseEntity<Compra>(compra,HttpStatus.OK);
+			return ResponseEntity.status(HttpStatus.CREATED).body(test.cartao);
 		}
 		
-		return new ResponseEntity<Compra>(compra,HttpStatus.CONFLICT);
-		
+		return ResponseEntity.status(400).build();
 	}
-	
+
+	@PostMapping(produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public ResponseEntity<Compra> salvar(@Valid Compra compra, User user,
+			@RequestParam("tipoPagamento") TipoPagamento tipoPagamento) {
+
+		compra.setDataCompra(new Date());
+		compra.setValor(compra.getValor() * compra.getQuantidade());
+
+		if (tipoPagamento.equals(TipoPagamento.CARTAO)) {
+
+			compra.setId(compra.getId());
+			compraService.salvarCompra(compra);
+			return new ResponseEntity<Compra>(compra, HttpStatus.OK);
+
+		} else if (tipoPagamento.equals(TipoPagamento.BOLETO)) {
+
+			compra.setId(compra.getId());
+			compraService.salvarCompra(compra);
+			return new ResponseEntity<Compra>(compra, HttpStatus.OK);
+		}
+
+		return new ResponseEntity<Compra>(compra, HttpStatus.CONFLICT);
+
+	}
+
 	@GetMapping(produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-	public ResponseEntity<Compra> detalhes(Principal principal){
+	public ResponseEntity<Compra> detalhes(Principal principal) {
 		Long id_compra = userService.getEmail(principal.getName()).getId();
 		Compra compra = compraService.findByIdCompra(id_compra);
-		if(compra != null) {
+		if (compra != null) {
 			return ResponseEntity.ok(compra);
 		}
 		return ResponseEntity.notFound().build();
